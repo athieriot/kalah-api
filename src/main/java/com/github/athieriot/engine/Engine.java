69a21157ad1ec1c2cb83885705618806c31906e5
Aurelien Thieriot <Aurelien.Thieriot@monitise.com>
2017-07-18 12:29:42 +0100

    Add moves

--- src/main/java/com/github/athieriot/engine/Engine.java
+++ src/main/java/com/github/athieriot/engine/Engine.java
@@ -25,11 +25,8 @@ public class Engine {
     }
 
     public void play(int player, int house) {
-        if (player < 1 || player > 2) {
-            throw new GameException("This is 2 players game only");
-        } else if (player != playerTurn) {
-            throw new GameException("Not your turn yet");
-        }
+        if (player < 1 || player > 2) {  throw new GameException("This is 2 players game only"); }
+        else if (player != playerTurn) { throw new GameException("Not your turn yet"); }
 
         board.move(player, house);
         togglePlayerTurn();
